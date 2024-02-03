2fb8c09479428fb0f5b40032f0ffb865b268aa2e
Amber Younus <amberyounus22@gmail.com>
2024-02-02 22:25:34 -0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import { useState } from "react";
 
 function App() {
   const [firstName, setFirstName] = useState();
   const [lastName, setlastName] = useState();
+  const [hobbies, setHobbies] = useState();
 
   return (
     <>
-      <div className="App">
+      <div className="firstName">
         <label htmlFor="">First Name</label>
-        <input type="text" value={firstName} onChange={e=>setFirstName(e.target.value)}/>
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
       </div>
-      <div>
+      <div className="lastName">
         <label htmlFor="">Last Name</label>
-        <input type="text" value={lastName} onChange={e=>setlastName(e.target.value)} />
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setlastName(e.target.value)}
+        />
       </div>
       <button>Add</button>
     </>
