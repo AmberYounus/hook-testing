8ac0adfd1bef9eea656bf88f84ff887bc9d827c1
Amber Younus <amberyounus22@gmail.com>
2024-02-05 22:34:26 -0800

    commit

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -2,21 +2,26 @@ import { useState, useEffect } from "react";
 
 function uselocalStorage() {
   const [value, setValue] = useState((key, initialValue) => {
-    
     const localValue = localStorage.getItem(key);
-    
-    if (localValue == null) {
 
+    if (localValue == null) {
       if (typeof initialValue === "function") {
         return initialValue();
       } else {
         return initialValue;
       }
-      
     } else {
       return JSON.parse(localValue);
     }
   });
+
+  useEffect(() => {
+    if (value === undefined) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [value, key]);
 }
 
 export default uselocalStorage;
